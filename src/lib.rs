be5e517ce980ac8f6fb5816cb86ed9e0254c3ff2
yds12 <28710965+yds12@users.noreply.github.com>
2020-10-23 20:20:53 +0200

    Struct and some initial functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,39 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+  use super::*;
+
+  #[test]
+  fn new_vec_has_len_zero() {
+    let bitvec = BitVec::new();
+    assert_eq!(bitvec.len(), 0);
+  }
+}
+
+pub struct BitVec {
+  data: Vec<u64>,
+  length: usize
+}
+
+impl BitVec {
+  pub fn new() -> Self {
+    BitVec {
+      data: Vec::new(),
+      length: 0
     }
+  }
+
+  fn add_block(self: &mut Self) {
+    self.data.insert(0, 0);
+  }
+
+  pub fn len(self: Self) -> usize {
+    self.length
+  }
+
+  pub fn push(self: &mut Self, value: u8) {
+    if self.data.len() * 64 < self.length + 1 {
+      self.add_block();
+    }
+    self.length += 1;
+  }
 }
